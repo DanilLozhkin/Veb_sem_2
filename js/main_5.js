80dbbe48c893402fdd639ed29604d0b18fb71826
DanilLozhkin <117292325+DanilLozhkin@users.noreply.github.com>
2023-03-26 19:06:46 +0300

    d

--- js/main_5.js
+++ js/main_5.js
@@ -1,1 +1,17 @@
-console.log("dfxаваидлиа");
+//const { error } = require('console');
+const http = require('http')
+
+const hostname = '127.0.0.1';
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    //console.log(req.headers['x-forwarded-for'] || req.socket.remoteAddress || null);
+    console.log(req.);
+});
+
+server.listen(PORT, hostname, (error) => {
+    error ? console.log(error) : console.log(`listening port ${PORT}`);
+});
+
+//    127.0.0.1:3000
+//   node js/main_5
