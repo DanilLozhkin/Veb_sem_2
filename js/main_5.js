fabe30dc5be107c6d81567c6915b42ab4662b54b
DanilLozhkin <117292325+DanilLozhkin@users.noreply.github.com>
2023-03-29 23:13:07 +0300

    свчс

--- js/main_5.js
+++ js/main_5.js
@@ -1,6 +1,8 @@
 //const { error } = require('console');
-const http = require('http')
-//let data
+const http = require('http');
+const { parse } = require('querystring');
+
+let date='';
 
 const hostname = '127.0.0.1';
 const PORT = 3000;
@@ -8,16 +10,25 @@ const PORT = 3000;
 const server = http.createServer((req, res) => {
     console.log(req.method,req.url,"/",req.httpVersion );
     console.log("HOST:",req.socket.address().address, ":",req.socket.address().port);
-    res.end(`HI`);
-//reg.on('data', (chank)=>
-// data+=chank;
-//)
-//reg.on('end',  ()=>{
-//     comments.push(data);
-//     res.setHeader('gfh',"eeefew/json");
-//     res.end(JSON.stringify(comments));
-// })
-    
+    if(req.method=='GET'){
+        res.end(`HI`);
+        if(req.url=="/stats"){
+
+        }
+    }else if(req.method=='POST'){
+        if(req.url=="/comments"){
+            let body='';
+            req.on('data', chunk => {
+                body +=chunk;
+            });
+            req.on('end',  ()=>{
+                let params = parse(body);
+                let json = JSON.stringify(params);
+                date +=json + ' ';
+                res.end(date);
+            });
+        }
+    }
 });
 
 server.listen(PORT, hostname, (error) => {
