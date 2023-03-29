1afb54d4253bf5823bb65cbe98d25b6a1d088742
DanilLozhkin <danil2003445@gmail.com>
2023-03-29 19:34:49 +0300

    и

--- js/main_5.js
+++ js/main_5.js
@@ -1,5 +1,6 @@
 //const { error } = require('console');
 const http = require('http')
+//let data
 
 const hostname = '127.0.0.1';
 const PORT = 3000;
@@ -8,6 +9,14 @@ const server = http.createServer((req, res) => {
     console.log(req.method,req.url,"/",req.httpVersion );
     console.log("HOST:",req.socket.address().address, ":",req.socket.address().port);
     res.end(`HI`);
+//reg.on('data', (chank)=>
+// data+=chank;
+//)
+//reg.on('end',  ()=>{
+//     comments.push(data);
+//     res.setHeader('gfh',"eeefew/json");
+//     res.end(JSON.stringify(comments));
+// })
     
 });
 
