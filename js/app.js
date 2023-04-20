7accb1fe0b7eabc7760678093865c24bf74894bc
DanilLozhkin <117292325+DanilLozhkin@users.noreply.github.com>
2023-04-20 12:04:24 +0300

    1

--- js/app.js
+++ js/app.js
@@ -1,20 +1,13 @@
 
 const express = require('express');
-<<<<<<< HEAD
 const morgan = require('morgan');
 const helmet = require('helmet');
 
 const app = express();
 const router = require('./main_6');
 
-//app.disable('x-powered-by');
-app.use(helmet());
-=======
-const app = express();
-const router = require('./main_6');
-
->>>>>>> 1fda1ecdb3779cb932491c57e513646793f2d6ac
-
+app.disable('x-powered-by');
+app.use(helmet())
 const Key = (req, res, next) => {
     const apiKey = req.query.apiKey; 
     if (!apiKey || apiKey !== 'myApiKey') { 
@@ -27,7 +20,6 @@ app.use(express.static('public'));
 app.use(express.json());
 
 app.use((req, res, next) => {
-<<<<<<< HEAD
     //console.log(req.method, req.url, "/", req.httpVersion);
     console.log("HOST:", req.socket.address().address, ":", req.socket.address().port);
     next();
@@ -37,16 +29,6 @@ app.use(morgan('dev'));
 app.use('/', router);
 
 
-=======
-    console.log(req.method, req.url, "/", req.httpVersion);
-    console.log("HOST:", req.socket.address().address, ":", req.socket.address().port);
-    next();
-});
-
-app.use('/', router);
-
-// Используем middleware функцию для авторизации на маршруте /api/users
->>>>>>> 1fda1ecdb3779cb932491c57e513646793f2d6ac
 app.get('/api/users', Key, function (req, res) {
     const user = req.query.id;
     res.send({
